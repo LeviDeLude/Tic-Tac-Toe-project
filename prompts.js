27941eb6f12fd9c726603586802084b5fcdf11ea
Levi.DeLude <levi.delude@fn3545.optiv.com>
2016-06-23 12:21:02 -0600

    Inital commit

---
+++ prompts.js
@@ -0,0 +1,32 @@
+var prompt = require('prompt');
+
+var player1 = prompt.get("Are you ready to play, Player 1?");
+if (player1 === "yes"){
+  console.log("Great! Let's wait for your oponent Player 1!");
+}
+else{
+  console.log("That's fine! Come back when your ready.");
+}
+/*
+var player2 = prompt("Are you ready to play, Player 2?")
+if (player2 === "yes") {
+  console.log("Awesome! Let's get started player 2.");
+}
+else {
+  console.log("Alright! Just come back when you want to play.");
+}
+
+var choice1 = prompt("Player 1, do you want to be X or O?")
+if (choice1 === "X" || choice === "x") { //How do I make it either or?
+  console.log("Great! You will go first")
+}
+else {
+  console.log("Good choice, but Player 2 will be going first.");
+}
+*/
+//charAt
+//matrix .. nestesd matrices
+//while loop
+console.log("|X| |");
+console.log("| | |");
+console.log("| | |");
