8c23e9baaa51c9ca5e45cf04f3b9c4192fe16374
Levi DeLude <levigdelude@gmail.com>
2016-06-24 00:30:07 -0600

    An outline for the X's and O's

---
+++ GameCharaters.js
@@ -0,0 +1,16 @@
+//I wasn't too sure how to exactly put this into code
+//I am just going to comment it out and around it till i firgure it out.
+
+/*
+var symbal = X;
+
+if (mouseClickedTotal === odd number) { //This is how player 1 and 2 will take turns.
+  symbal === X;
+}
+else {
+  symbal === O;
+}
+
+if (mouseClicked in region) { //Region of the board
+  console.log(symbal);
+*/
