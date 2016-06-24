90bfe6cadc46e84369c0c9428966d9a90d4a0d0c
Levi DeLude <levigdelude@gmail.com>
2016-06-23 23:55:28 -0600

    Changed prompt() to prompt.get()

--- prompts.js
+++ prompts.js
@@ -7,8 +7,8 @@ if (player1 === "yes"){
 else{
   console.log("That's fine! Come back when your ready.");
 }
-/*
-var player2 = prompt("Are you ready to play, Player 2?")
+
+var player2 = prompt.get("Are you ready to play, Player 2?")
 if (player2 === "yes") {
   console.log("Awesome! Let's get started player 2.");
 }
@@ -16,17 +16,13 @@ else {
   console.log("Alright! Just come back when you want to play.");
 }
 
-var choice1 = prompt("Player 1, do you want to be X or O?")
-if (choice1 === "X" || choice === "x") { //How do I make it either or?
+var choice1 = prompt.get("Player 1, do you want to be X or O?")
+if (choice1 === "X" || choice === "x") {
   console.log("Great! You will go first")
 }
 else {
   console.log("Good choice, but Player 2 will be going first.");
 }
-*/
-//charAt
-//matrix .. nestesd matrices
-//while loop
-console.log("|X| |");
-console.log("| | |");
-console.log("| | |");
+
+var start = prompt.get("Type ok to start!");
+var oops = prompt.get("Sorry I forgot, is player 1 going first or is player 2 to going first.")
