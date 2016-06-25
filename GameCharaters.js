fd84590fbd6a8532f602fad59e3abf299ccee6a4
Levi DeLude <levigdelude@gmail.com>
2016-06-24 23:51:32 -0600

    Changing the code from a mouse click game to then variables instead that the users will fill out with prompt.get commands

--- GameCharaters.js
+++ GameCharaters.js
@@ -1,16 +1,15 @@
-//I wasn't too sure how to exactly put this into code
-//I am just going to comment it out and around it till i firgure it out.
+console.log("The game board will be lined up in a way such as this,");
+console.log("|1|2|3|");
+console.log("|4|5|6|");
+console.log("|7|8|9|");
+console.log("Remember this order because this will be how the game is played.");
 
-/*
-var symbal = X;
-
-if (mouseClickedTotal === odd number) { //This is how player 1 and 2 will take turns.
-  symbal === X;
-}
-else {
-  symbal === O;
-}
-
-if (mouseClicked in region) { //Region of the board
-  console.log(symbal);
-*/
+var turn1 = prompt.get("Player 1, which space would you like to go in first?")
+var turn2 = prompt.get("Player 2, which space would you like to go in?")
+var turn3 = prompt.get("Player 1, which space would you like to go in?")
+var turn4 = prompt.get("Player 2, which space would you like to go in?")
+var turn5 = prompt.get("Player 1, which space would you like to go in?")
+var turn6 = prompt.get("Player 2, which space would you like to go in?")
+var turn7 = prompt.get("Player 1, which space would you like to go in?")
+var turn8 = prompt.get("Player 2, which space would you like to go in?")
+var turn9 = prompt.get("Player 1, which space would you like to go in?")
