d9e455f921522e89122fc7d7653d120f5743ee89
Levi DeLude <levigdelude@gmail.com>
2016-06-27 22:09:31 -0600

    Setting up the skeleton for the turns and setting the game characters in their corresponding spaces

---
+++ turns.js
@@ -0,0 +1,260 @@
+//Turn 1.
+if (turn1 = 1) {
+  space1 = "x";
+}
+if (turn1 = 2) {
+  space2 = "x";
+}
+if (turn1 = 3) {
+  space3 = "x";
+}
+if (turn1 = 4) {
+  space4 = "x";
+}
+if (turn1 = 5) {
+  space5 = "x";
+}
+if (turn1 = 6) {
+  space6 = "x";
+}
+if (turn1 = 7) {
+  space7 = "x";
+}
+if (turn1 = 8) {
+  space8 = "x";
+}
+if (turn1 = 9) {
+  space9 = "x";
+}
+
+//Turn 2
+if (turn2 = 1) {
+  space1 = "o";
+}
+if (turn2 = 2) {
+  space2 = "o";
+}
+if (turn2 = 3) {
+  space3 = "o";
+}
+if (turn2 = 4) {
+  space4 = "o";
+}
+if (turn2 = 5) {
+  space5 = "o";
+}
+if (turn2 = 6) {
+  space6 = "o";
+}
+if (turn2 = 7) {
+  space7 = "o";
+}
+if (turn2 = 8) {
+  space8 = "o";
+}
+if (turn2 = 1) {
+  space1 = "o";
+}
+
+//Turn 3
+if (turn3 = 1) {
+  space1 = "x";
+}
+if (turn3 = 2) {
+  space2 = "x";
+}
+if (turn3 = 3) {
+  space3 = "x";
+}
+if (turn3 = 4) {
+  space4 = "x";
+}
+if (turn3 = 5) {
+  space5 = "x";
+}
+if (turn3 = 6) {
+  space6 = "x";
+}
+if (turn3 = 7) {
+  space7 = "x";
+}
+if (turn3 = 8) {
+  space8 = "x";
+}
+if (turn3 = 9) {
+  space9 = "x";
+}
+
+//Turn 4
+if (turn4 = 1) {
+  space1 = "o";
+}
+if (turn4 = 2) {
+  space2 = "o";
+}
+if (turn4 = 3) {
+  space3 = "o";
+}
+if (turn4 = 4) {
+  space4 = "o";
+}
+if (turn4 = 5) {
+  space5 = "o";
+}
+if (turn4 = 6) {
+  space6 = "o";
+}
+if (turn4 = 7) {
+  space7 = "o";
+}
+if (turn4 = 8) {
+  space8 = "o";
+}
+if (turn4 = 9) {
+  space9 = "o";
+}
+
+//Turn 5
+if (turn5 = 1) {
+  space1 = "x";
+}
+if (turn5 = 2) {
+  space2 = "x";
+}
+if (turn5 = 3) {
+  space3 = "x";
+}
+if (turn5 = 4) {
+  space4 = "x";
+}
+if (turn5 = 5) {
+  space5 = "x";
+}
+if (turn5 = 6) {
+  space6 = "x";
+}
+if (turn5 = 7) {
+  space7 = "x";
+}
+if (turn5 = 8) {
+  space8 = "x";
+}
+if (turn5 = 9) {
+  space9 = "x";
+}
+
+//Turn 6
+if (turn6 = 1) {
+  space1 = "o";
+}
+if (turn6 = 2) {
+  space2 = "o";
+}
+if (turn6 = 3) {
+  space3 = "o";
+}
+if (turn6 = 4) {
+  space4 = "o";
+}
+if (turn6 = 5) {
+  space5 = "o";
+}
+if (turn6 = 6) {
+  space6 = "o";
+}
+if (turn6 = 7) {
+  space7 = "o";
+}
+if (turn6 = 8) {
+  space8 = "o";
+}
+if (turn6 = 9) {
+  space9 = "o";
+}
+
+//Turn 7
+if (turn7 = 1) {
+  space1 = "x";
+}
+if (turn7 = 2) {
+  space2 = "x";
+}
+if (turn7 = 3) {
+  space3 = "x";
+}
+if (turn7 = 4) {
+  space4 = "x";
+}
+if (turn7 = 5) {
+  space5 = "x";
+}
+if (turn7 = 6) {
+  space6 = "x";
+}
+if (turn7 = 7) {
+  space7 = "x";
+}
+if (turn7 = 8) {
+  space8 = "x";
+}
+if (turn7 = 9) {
+  space9 = "x";
+}
+
+//Turn 8
+if (turn8 = 1) {
+  space1 = "o";
+}
+if (turn8 = 2) {
+  space2 = "o";
+}
+if (turn8 = 3) {
+  space3 = "o";
+}
+if (turn8 = 4) {
+  space4 = "o";
+}
+if (turn8 = 5) {
+  space5 = "o";
+}
+if (turn8 = 6) {
+  space6 = "o";
+}
+if (turn8 = 7) {
+  space7 = "o";
+}
+if (turn8 = 8) {
+  space8 = "o";
+}
+if (turn8 = 9) {
+  space9 = "o";
+}
+
+//Turn 9
+if (turn9 = 1) {
+  space1 = "x";
+}
+if (turn9 = 2) {
+  space2 = "x";
+}
+if (turn9 = 3) {
+  space3 = "x";
+}
+if (turn9 = 4) {
+  space4 = "x";
+}
+if (turn9 = 5) {
+  space5 = "x";
+}
+if (turn9 = 6) {
+  space6 = "x";
+}
+if (turn9 = 7) {
+  space7 = "x";
+}
+if (turn9 = 8) {
+  space8 = "x";
+}
+if (turn9 = 9) {
+  space9 = "x";
+}
