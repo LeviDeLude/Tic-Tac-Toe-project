734e5a9ae71a961b5e7b0527cea3364dfd751b7a
Levi DeLude <levigdelude@gmail.com>
2016-06-28 12:16:50 -0600

    Code add to tell a player the space is already tooken and the need to choose another spot to go.

---
+++ incorectSpaces.js
@@ -0,0 +1,108 @@
+var retry = prompt.get("Where would you rather go?");
+//Space 1
+if (space1 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space1 = "x";
+  console.log(retry);
+}
+else if (space1 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space1 = "o";
+  console.log(retry);
+}
+
+//Space 2
+if (space2 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space2 = "x";
+  console.log(retry);
+}
+else if (space2 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space2 = "o";
+  console.log(retry);
+}
+
+//Space 3
+if (space3 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space3 = "x";
+  console.log(retry);
+}
+else if (space3 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space3 = "o";
+  console.log(retry);
+}
+
+//Space 4
+if (space4 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space4 = "x";
+  console.log(retry);
+}
+else if (space4 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space4 = "o";
+  console.log(retry);
+}
+
+//Space 5
+if (space5 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space5 = "x";
+  console.log(retry);
+}
+else if (space5 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space5 = "o";
+  console.log(retry);
+}
+
+//Space 6
+if (space6 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space6 = "x";
+  console.log(retry);
+}
+else if (space6 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space6 = "o";
+  console.log(retry);
+}
+
+//Space 7
+if (space7 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space7 = "x";
+  console.log(retry);
+}
+else if (space7 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space7 = "o";
+  console.log(retry);
+}
+
+//Space 8
+if (space8 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space8 = "x";
+  console.log(retry);
+}
+else if (space8 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space8 = "o";
+  console.log(retry);
+}
+
+//Space 9
+if (space9 === "x") {
+  console.log("That space has already been used, try a diffent one!");
+  space9 = "x";
+  console.log(retry);
+}
+else if (space9 === "o") {
+  console.log("That space has already been used, try a diffent one!");
+  space9 = "o";
+  console.log(retry);
+}
