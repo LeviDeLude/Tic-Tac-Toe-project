3aa5849018e6d33a3a5e5d40c662143ca809768e
Levi DeLude <levigdelude@gmail.com>
2016-07-05 10:45:31 -0600

    Fixing my prompts to work with the user and logging the board after each turn.

--- GameCharaters.js
+++ GameCharaters.js
@@ -1,15 +1,26 @@
+var prompt = require('readline-sync');
+
 console.log("The game board will be lined up in a way such as this,");
 console.log("|1|2|3|");
 console.log("|4|5|6|");
 console.log("|7|8|9|");
 console.log("Remember this order because this will be how the game is played.");
 
-var turn1 = prompt.get("Player 1, which space would you like to go in first?")
-var turn2 = prompt.get("Player 2, which space would you like to go in?")
-var turn3 = prompt.get("Player 1, which space would you like to go in?")
-var turn4 = prompt.get("Player 2, which space would you like to go in?")
-var turn5 = prompt.get("Player 1, which space would you like to go in?")
-var turn6 = prompt.get("Player 2, which space would you like to go in?")
-var turn7 = prompt.get("Player 1, which space would you like to go in?")
-var turn8 = prompt.get("Player 2, which space would you like to go in?")
-var turn9 = prompt.get("Player 1, which space would you like to go in?")
+var turn1 = prompt.question("Player 1, which space would you like to go in first?")
+console.log(logGameBoard);
+var turn2 = prompt.question("Player 2, which space would you like to go in?")
+console.log(logGameBoard);
+var turn3 = prompt.question("Player 1, which space would you like to go in?")
+console.log(logGameBoard);
+var turn4 = prompt.question("Player 2, which space would you like to go in?")
+console.log(logGameBoard);
+var turn5 = prompt.question("Player 1, which space would you like to go in?")
+console.log(logGameBoard);
+var turn6 = prompt.question("Player 2, which space would you like to go in?")
+console.log(logGameBoard);
+var turn7 = prompt.question("Player 1, which space would you like to go in?")
+console.log(logGameBoard);
+var turn8 = prompt.question("Player 2, which space would you like to go in?")
+console.log(logGameBoard);
+var turn9 = prompt.question("Player 1, which space would you like to go in?")
+console.log(logGameBoard);
