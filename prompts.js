32eaf2618a49448f2f2d0fa26be07fd7a8f558f6
Levi DeLude <levigdelude@gmail.com>
2016-07-05 10:16:01 -0600

    Changing my program to run working user prompts

--- prompts.js
+++ prompts.js
@@ -1,6 +1,6 @@
-var prompt = require('prompt');
+var prompt = require('readline-sync');
 
-var player1 = prompt.get("Are you ready to play, Player 1?");
+var player1 = prompt.question("Are you ready to play, Player 1?");
 if (player1 === "yes"){
   console.log("Great! Let's wait for your oponent Player 1!");
 }
@@ -8,7 +8,7 @@ else{
   console.log("That's fine! Come back when your ready.");
 }
 
-var player2 = prompt.get("Are you ready to play, Player 2?")
+var player2 = prompt.question("Are you ready to play, Player 2?")
 if (player2 === "yes") {
   console.log("Awesome! Let's get started player 2.");
 }
@@ -16,7 +16,7 @@ else {
   console.log("Alright! Just come back when you want to play.");
 }
 
-var choice1 = prompt.get("Player 1, do you want to be X or O?")
+var choice1 = prompt.question("Player 1, do you want to be X or O?")
 if (choice1.toLowerCase === "x") {
   console.log("Great! You will go first")
 }
@@ -24,5 +24,5 @@ else {
   console.log("Good choice, but Player 2 will be going first.");
 }
 
-var start = prompt.get("Type ok to start!");
-var oops = prompt.get("Sorry I forgot, is player 1 going first or is player 2 to going first.")
+var start = prompt.question("Type ok to start!");
+var oops = prompt.question("Sorry I forgot, is player 1 going first or is player 2 to going first.")
